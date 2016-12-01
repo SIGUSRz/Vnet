9d2fae4f9cd4d1ec5cc4080d2451d263cc02a71b
sz1358 <zengs@172-16-119-201.DYNAPOOL.NYU.EDU>
2016-11-30 23:02:18 -0500

    Commit #7
    Add run.sh
    Try to fix download problem

--- data/download.sh
+++ data/download.sh
@@ -1,10 +1,10 @@
-wget -c http://visualqa.org/data/mscoco/vqa/Annotations_Train_mscoco.zip
-wget -c http://visualqa.org/data/mscoco/vqa/Questions_Train_mscoco.zip
-wget -c http://msvocds.blob.core.windows.net/coco2014/train2014.zip
+wget -c http://visualqa.org/data/mscoco/vqa/Annotations_Train_mscoco.zip --proxy on
+wget -c http://visualqa.org/data/mscoco/vqa/Questions_Train_mscoco.zip --proxy on
+wget -c http://msvocds.blob.core.windows.net/coco2014/train2014.zip -- proxy on
 
-wget -c http://visualqa.org/data/mscoco/vqa/Annotations_Val_mscoco.zip
-wget -c http://visualqa.org/data/mscoco/vqa/Questions_Val_mscoco.zip
-wget -c http://msvocds.blob.core.windows.net/coco2014/val2014.zip
+wget -c http://visualqa.org/data/mscoco/vqa/Annotations_Val_mscoco.zip --proxy on
+wget -c http://visualqa.org/data/mscoco/vqa/Questions_Val_mscoco.zip --proxy on
+wget -c http://msvocds.blob.core.windows.net/coco2014/val2014.zip --proxy on
 
 unzip Annotations_Train_mscoco.zip
 unzip Questions_Train_mscoco.zip
