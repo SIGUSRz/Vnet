82c0a3a3f2766ecb021e74a474aabe461be3f76e
sz1358 <zengs@172-16-119-58.DYNAPOOL.NYU.EDU>
2016-12-17 06:25:40 -0500

    Commit #15
    Finish Answer Generator
    Waiting for Debug

--- answer_generator.py
+++ answer_generator.py
@@ -100,8 +100,19 @@ class Answer_Generator():
                                                         name='que_att_W')
         self.que_att_b = tf.Variable(tf.random_uniform([self.max_que_length, 1], \
                                                         -self.init_bound, self.init_bound),
-                                                        name='img_W')
-
+                                                        name='que_att_b')
+        self.drop_W = tf.Variable(tf.random_uniform([self.rnn_size, self.hidden_dim], \
+                                                        -self.init_bound, self.init_bound),
+                                                        name='drop_W')
+        self.drop_b = tf.Variable(tf.random_uniform([self.hidden_dim], \
+                                                        -self.init_bound, self.init_bound),
+                                                        name='drop_b')
+        self.score_W = tf.Variable(tf.random_uniform([self.hidden_dim, self.num_output], \
+                                                        -self.init_bound, self.init_bound),
+                                                        name='score_W')
+        self.score_b = tf.Variable(tf.random_uniform([self.num_output], \
+                                                        -self.init_bound, self.init_bound),
+                                                        name='score_b')
     def train_coattention_model(self):
         img_state = tf.placeholder('float32', [None, self.img_dim[0], self.img_dim[1], self.img_dim[2]], name='img_state')
         img_state = tf.contrib.layers.batch_norm(img_state)
@@ -111,9 +122,19 @@ class Answer_Generator():
         que_state, sentence_batch = self.que_processor.train_hierarchy_cell()
         with tf.variable_scope('attention'):
             img_attention, que_attention, new_img_state, new_que_state, hidden_state = self.add_attention(img_state, que_state)
-            
-        loss = 0.0
-        return loss, img_state, sentence_batch, label_batch
+        score = tf.tanh(tf.matmul(img_attention, self.score_W) + tf.matmul(que_attention, self.score_W) + self.score_b)
+        score = tf.nn.dropout(score, 1 - self.dropout_rate)
+        logits = tf.relu(tf.nn.xw_plus_b(score, self.score_W, self.score_b))
+        # logits = tf.contrib.layers.batch_norm(logtis)
+
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, label_batch, name='entropy')
+        ans_probability = tf.nn.softmax(logits, name='answer_prob')
+
+        predict = tf.argmax(ans_probability, 1)
+        correct_predict = tf.equal(tf.argmax(ans_probability, 1), tf.argmax(label_batch, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_predict, tf.float32))
+        loss = tf.reduce_sum(cross_entropy, name='loss')
+        return loss, accuracy, predict, img_state, sentence_batch, label_batch
 
     def add_attention(self, img_state, que_state, hidden_state=None):
         update = None
